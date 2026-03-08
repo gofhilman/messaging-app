8aea16797c00044a3561f68ff1a9cc0b2b88694a
Hilman Fikry <hilman12fikry@gmail.com>
2026-03-08 16:48:01 +0700

    feat: add text post route

---
+++ backend/controllers/messagesController.ts
@@ -0,0 +1,17 @@
+import { prisma } from "../lib/prisma";
+
+async function textPost(req: any, res: any) {
+  const { chatId, text } = req.body;
+  const message = await prisma.message.create({
+    data: {
+      chatId,
+      text,
+      type: "TEXT",
+      userId: req.user.id,
+    },
+  });
+}
+
+async function imagePost(req: any, res: any) {}
+
+export { textPost, imagePost };
--- backend/routes/messagesRouter.ts
+++ backend/routes/messagesRouter.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 const messagesRouter = Router();
 
-messagesRouter.post("/");
+messagesRouter.post("/text");
+messagesRouter.post("/image");
 
 export default messagesRouter;
