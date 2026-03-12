444a5edc36a4e3a7546b11d3ce04c2023ce3c8bc
Hilman Fikry <hilman12fikry@gmail.com>
2026-03-13 00:41:51 +0700

    fix: use direct_url in prisma config

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -10,9 +10,9 @@ export default defineConfig({
   },
   datasource: {
     url:
-      process.env["DATABASE_URL"] ??
+      process.env["DIRECT_URL"] ??
       (() => {
-        throw new Error("DATABASE_URL missing");
+        throw new Error("DIRECT_URL missing");
       })(),
   },
 });
